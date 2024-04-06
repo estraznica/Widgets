11a89cfb5134e5f77b938f461901161249a7f6f2
Aleksandra Ablov <ablovatskayaalek@gmail.com>
2024-04-06 21:27:04 +0500

    поправила прокрутку

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -14,7 +14,11 @@ export default function Widget(props: Props) {
   const { widget, deleteWidget } = props;
   const widgetRef = React.useRef<HTMLDivElement>(null);
   React.useEffect(() => {
-    const handleDrag = () => {
+    const handleDrag = (event: MouseEvent) => {
+      //кнопка мыши не нажата - значит не перетаскиваем
+      if (event.buttons !== 1) {
+        return;
+      }
       //прокрутка окна во время перетаскивания
       const widgetRect = widgetRef.current?.getBoundingClientRect();
       if (widgetRect) {
@@ -46,14 +50,14 @@ export default function Widget(props: Props) {
     const currentRef = widgetRef.current;
     currentRef?.addEventListener('dragstart', handleDragStart);
     currentRef?.addEventListener('dragend', handleDragEnd);
-    currentRef?.addEventListener('drag', handleDrag);
+    currentRef?.addEventListener('mousemove', handleDrag);
 
     return () => {
       currentRef?.removeEventListener('dragstart', handleDragStart);
       currentRef?.removeEventListener('dragend', handleDragEnd);
-      currentRef?.removeEventListener('drag', handleDrag);
+      currentRef?.removeEventListener('mousemove', handleDrag);
     };
-  }, []);
+  }, [widgetRef.current]);
   return (
     <>
       <div className="widget" ref={widgetRef} draggable="true" data-widget-id={widget.id}>
